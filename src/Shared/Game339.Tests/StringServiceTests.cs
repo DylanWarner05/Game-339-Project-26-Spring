78681663477a4792b48d35958333ef47e45a3f01
DylanWarner05 <dylan.m.warner714@gmail.com>
2026-02-18 23:20:34 -0800

    trying to make test recognize terms

--- src/Shared/Game339.Tests/StringServiceTests.cs
+++ src/Shared/Game339.Tests/StringServiceTests.cs
@@ -42,7 +42,7 @@ public class StringServiceTests
     [TestCase("a b c", "c b a")] // multiple spaces
     public void ReverseWords_CommonCases(string input, string expected)
     {
-        var actual = _vsc.ReverseWords(input);
+        var actual = StringService.ReverseWords(input);
         Assert.That(actual, Is.EqualTo(expected));
     }
 }
